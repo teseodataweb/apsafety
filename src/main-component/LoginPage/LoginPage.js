1a9d95c87ac76516119f634b8f463ccdab7a23f2
IrvingAlvarez12 <irving.4tocreativo@gmail.com>
2025-07-24 14:25:34 -0600

    cambios productos

--- src/main-component/LoginPage/LoginPage.js
+++ src/main-component/LoginPage/LoginPage.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react';
 import Loginpage from '../../components/Loginpage/Loginpage'
-import FooterS3 from '../../components/footerS3/FooterS3';
 import CursorMaus from '../../components/CursorMaus/CursorMaus';
 const LoginPage = () => {    return (
         <Fragment>
             <Loginpage />
-            <FooterS3 />
             <CursorMaus />
         </Fragment>
     )
